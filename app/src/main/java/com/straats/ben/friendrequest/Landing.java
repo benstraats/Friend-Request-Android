751a95e3ce0f423ad93f46a8b8b1f76455857221
Ben Straatsma <benstraatsma@gmail.com>
2018-04-15 14:47:24 -0400

    Removed shit from landing and added a bunch of custom rows for landing pages main table

--- app/src/main/java/com/straats/ben/friendrequest/Landing.java
+++ app/src/main/java/com/straats/ben/friendrequest/Landing.java
@@ -25,54 +25,12 @@ import java.util.ArrayList;
 
 public class Landing extends AppCompatActivity {
 
-    private ListView mainListView;
-
-    private ArrayList<String> users = new ArrayList<String>();
-    private ArrayAdapter<String> adapter;
-
-    private JSONArray friendUsers;
-
-    private ProgressBar loadingBar;
-
-    private boolean doneUserLoad = true;
-    private boolean currentlyLoading = false;
-    private int userLoadSkip = 0;
-    private int userLoadLimit = 49;
+    ProgressBar loadingBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing);
-
-        mainListView = findViewById(R.id.mainListView);
-        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, users);
-        mainListView.setAdapter(adapter);
-
-        mainListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
-                try {
-
-                    String user1 = friendUsers.getJSONObject(position).getString("user1");
-                    String user2 = friendUsers.getJSONObject(position).getString("user2");
-                    String friendID = friendUsers.getJSONObject(position).getString("_id");
-
-                    String friendUserID = user1;
-
-                    if (user1.equals(Utils.userID)) {
-                        friendUserID = user2;
-                    }
-
-                    Intent intent = new Intent(getApplicationContext(), Profile.class);
-                    intent.putExtra("friendUserID", friendUserID);
-                    intent.putExtra("friendID", friendID);
-                    startActivity(intent);
-                } catch (JSONException e) {
-                    //bad friend yo
-                }
-            }
-        });
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -98,92 +56,11 @@ public class Landing extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
-        mainListView.setOnScrollListener(new AbsListView.OnScrollListener() {
-            @Override
-            public void onScrollStateChanged(AbsListView view, int scrollState) {
-
-            }
-
-            @Override
-            public void onScroll(AbsListView view, int firstVisibleItem,
-                                 int visibleItemCount, int totalItemCount) {
-
-                final int lastItem = firstVisibleItem + visibleItemCount;
-
-                if(lastItem == totalItemCount && !doneUserLoad && !currentlyLoading) {
-                    userLoadSkip += userLoadLimit;
-                    getFriends(userLoadSkip, userLoadLimit);
-                }
-            }
-        });
     }
 
     @Override
     public void onResume() {
         super.onResume();
-        userLoadSkip = 0;
-        users.clear();
-        friendUsers = null;
-        doneUserLoad = false;
-        getFriends(userLoadSkip, userLoadLimit);
-    }
-
-    private void getFriends(final int skip, final int limit) {
-
-        currentlyLoading = true;
-        final VolleyWrapper vw = VolleyWrapper.getInstance(getApplicationContext());
-
-        VolleyWrapper.VolleyCallback callback = new VolleyWrapper.VolleyCallback() {
-            @Override
-            public void onSuccess(JSONObject response) {
-                hideLoading();
-                try {
-                    int total = Integer.parseInt(response.getString("total"));
-
-                    int numUsers = Math.min((total-skip), limit);
-
-                    friendUsers = Utils.combineArray(friendUsers, response.getJSONArray("data"));
-
-                    for (int i=skip; i<(skip+numUsers); i++) {
-                        String user1 = friendUsers.getJSONObject(i).getString("user1");
-                        String user2 = friendUsers.getJSONObject(i).getString("user2");
-
-                        JSONObject userInfo = response.getJSONObject("userInfo");
-
-                        if (user1.equals(Utils.userID)) {
-                            users.add(Utils.getUserInfo(user2, userInfo));
-                        } else {
-                            users.add(Utils.getUserInfo(user1, userInfo));
-                        }
-                    }
-
-                    currentlyLoading = false;
-
-                    if (numUsers < limit || (skip + numUsers) == total) {
-                        doneUserLoad = true;
-                    }
-
-                    adapter.notifyDataSetChanged();
-                } catch (JSONException e) {
-                    Toast.makeText(getApplicationContext(), R.string.bad_response,
-                            Toast.LENGTH_SHORT).show();
-                    currentlyLoading = false;
-                }
-            }
-
-            @Override
-            public void onFailure(VolleyError error) {
-                Toast.makeText(getApplicationContext(), Utils.decodeError(error), Toast.LENGTH_SHORT)
-                        .show();
-                hideLoading();
-                currentlyLoading = false;
-            }
-        };
-
-        String url = Utils.friendsURL + "?$limit=" + limit + "&$skip=" + skip;
-        showLoading();
-        vw.request(getApplication(), url, Utils.getFriendsTAG, Request.Method.GET, null, callback);
     }
 
     private void showLoading() {
