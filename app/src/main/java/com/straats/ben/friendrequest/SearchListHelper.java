fef4ccabe953a6c0d85afe8638711704289e36a4
Ben Straatsma <benstraatsma@gmail.com>
2018-04-20 14:19:28 -0400

    Updated manifest and added code to searchListHelper

--- app/src/main/java/com/straats/ben/friendrequest/SearchListHelper.java
+++ app/src/main/java/com/straats/ben/friendrequest/SearchListHelper.java
@@ -1,4 +1,31 @@
 package com.straats.ben.friendrequest;
 
+import android.content.Context;
+import android.widget.ProgressBar;
+import android.widget.TableLayout;
+
+import java.util.ArrayList;
+
 public class SearchListHelper {
+
+    private Context c;
+    private final TableLayout mainList;
+    private ProgressBar progressBar;
+
+    private int limit = 50;
+
+    private ArrayList<CustomRow> rowList;
+
+    public SearchListHelper(Context context, TableLayout list, ProgressBar pgBar) {
+        this.c = context;
+        this.mainList = list;
+        this.progressBar = pgBar;
+
+        rowList = new ArrayList<>();
+    }
+
+    private class CustomRow {
+        //TODO: fill out
+    }
+
 }
